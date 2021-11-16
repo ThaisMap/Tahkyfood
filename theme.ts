f8814af784d30d234ebcf539ff887678202d02fb
Thais <thais.marpi@gmail.com>
2021-11-16 20:42:37 -0300

    feat: configurações iniciais

---
+++ theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    vermelhoForte: '#f44336',
+    vermelhoSplash: '#f5564a',
+    vermelhoSuave:'#f6695e',
+    rosaMedio: '#faa19b',
+    rosaSuave: '#fdd9d7',
+    rosaBackground: '#feeceb',
+    texto: '#222222'
+  }
+}
